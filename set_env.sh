5d5049ebe4d52976bad1351d99f47efc6426f505
auwal <a4auwal@gmail.com>
2022-11-17 11:22:32 +0100

    update env files

--- set_env.sh
+++ set_env.sh
@@ -2,9 +2,9 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=udagram
 export POSTGRES_PASSWORD=udagram1234
-export POSTGRES_HOST=udagram-db.c7zgb8iamnl9.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=udagram.cb9kogmszi4b.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::my-udagram-bucket
+export AWS_BUCKET=arn:aws:s3:::udagram-4
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=production
